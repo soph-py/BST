668f28fafbe6bbf83841020e132f8c9b62a207d2
Sophia Tierney <sftierney@ucdavis.edu>
2021-12-10 10:39:01 -0800

    add test cases for get min and get max nodes

--- Trees/test/trees/test_bst.py
+++ Trees/test/trees/test_bst.py
@@ -45,8 +45,37 @@ class TestBST(unittest.TestCase):
         cmp_tree._num_nodes = 5
         self.assertNotEqual(tree, cmp_tree)
 
-    #def test_min_node(self) -> None:
+    def test_min_node(self) -> None:
+        tree = BST()
+        tree.add_value(100)
+        tree.add_value(80)
+        tree.add_value(200)
+        tree.add_value(90)
+        tree.add_value(70)
+        tree.add_value(60)
+        tree.add_value(220)
+        tree.add_value(45)
+        tree.add_value(230)
+        tree._num_nodes = 9
+        tree_min_exp = 45
+        tree_min_actual = tree.get_min_node()
+        self.assertEqual(tree_min_exp, tree_min_actual.value)
 
+    def test_max_node(self) -> None:
+        tree = BST()
+        tree.add_value(100)
+        tree.add_value(80)
+        tree.add_value(200)
+        tree.add_value(90)
+        tree.add_value(70)
+        tree.add_value(60)
+        tree.add_value(220)
+        tree.add_value(45)
+        tree.add_value(230)
+        tree._num_nodes = 9
+        tree_max_exp = 230
+        tree_max_actual = tree.get_max_node()
+        self.assertEqual(tree_max_exp, tree_max_actual.value)
 
 
 if __name__ == '__main__':
